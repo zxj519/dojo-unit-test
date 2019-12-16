83ecbc472ff4fe21e470fbd081aa69de2553df36
Chen Liang <liachen@thoughtworks.com>
2019-12-16 15:00:42 +0800

    first commit

---
+++ src/main/java/version0/Movie.java
@@ -0,0 +1,27 @@
+package version0;
+
+public class Movie {
+    public static final int CHILDREN = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int _priceCode) {
+        this.priceCode = _priceCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ src/main/java/version0/Rental.java
@@ -0,0 +1,19 @@
+package version0;
+
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
