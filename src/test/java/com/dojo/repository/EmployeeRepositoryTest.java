a9d08aca6410276462f015945cbd27047108866d
Xunjie Zhou <xjzhou@thoughtworks.com>
2020-03-13 16:50:30 +0800

    fix port forward

--- src/test/java/com/dojo/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/dojo/repository/EmployeeRepositoryTest.java
@@ -4,15 +4,16 @@ import com.dojo.domain.entity.Employee;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@ActiveProfiles({"ci-test", "local-test"})
+@AutoConfigureTestDatabase(replace= Replace.NONE)
 public class EmployeeRepositoryTest {
 
   @Autowired
